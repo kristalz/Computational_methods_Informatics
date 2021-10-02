b5f4572ac8b2e9b89c4b089dff78cf549c90741e
kristalz <90003165+kristalz@users.noreply.github.com>
2021-10-02 04:33:57 -0400

    Add files via upload

--- Scripts/HW2/HW2-Exercise3.py
+++ Scripts/HW2/HW2-Exercise3.py
@@ -8,26 +8,26 @@ print("average =", sum(weights) / len(weights))
 # Explain what went wrong (6 points). 
 
 
-There are probably three reasons that can lead to overstraining the user's memory usage by loading this text file. 
-First, it is probably that "my friend" has a 32-bit operating system. All 32-bit operating systems have a 4GB RAM limit. Therefore, it is very likely that "my friend's" operating system used all the memories when loading the file.  
-Second, other than the memory usage in storing data, loading data in Python also consumes overhead memory about information of loading the data into the list and related procedural information.
-When loading the data into list, Python also creates pointers that record the location of where each element locates in the computer. These pointers consumer extra memory as reference memory.
-Third, since the original "weights" information are string in the data. They are convereted to float objects, which consume more memories than float numbers. 
+# There are probably three reasons that can lead to overstraining the user's memory usage by loading this text file. 
+# First, it is probably that "my friend" has a 32-bit operating system. All 32-bit operating systems have a 4GB RAM limit. Therefore, it is very likely that "my friend's" operating system used all the memories when loading the file.  
+# Second, other than the memory usage in storing data, loading data in Python also consumes overhead memory about information of loading the data into the list and related procedural information.
+# When loading the data into list, Python also creates pointers that record the location of where each element locates in the computer. These pointers consumer extra memory as reference memory.
+# Third, since the original "weights" information are string in the data. They are convereted to float objects, which consume more memories than float numbers. 
 
 # I made a test file to demonstrate how loading the text file into list consume much more memories than we think. 
 # The "weights.txt" I created include a total of 30 random numbers. I used the code provided by "my firend" to load the file. 
 import sys
-sys.getsizeof(weights)
+print(sys.getsizeof(weights))
 
 # I then imported sys and used the function "getsizeof" to get the memory size of my list. It returned 312 bytes.
 
 # Then I wanted to see the size of individual element in my list. 
 
-sys.getsizeof(weights[0])
+print(sys.getsizeof(weights[0]))
 
 # It returned 24 bytes.
 
-len(weights)
+print(len(weights))
 
 # Since there are a total of 30 elements inside my list. The total size could be 24*30 = 720 bytes, which is almost 2.3 times the original size of the list. 
 # This means that Python almost doubled the size of my original data. Therefore, it is likely that "my friend"'s computer consumed more than 8 GB memories as loading the file and thus exceeded the RAM limit.
@@ -35,27 +35,28 @@ len(weights)
 # Suggest a way of storing all the data in memory that would work (7 points), 
 
 
-I suggest storing the list of data inside an array instead of a list, which can substantially reduce the overhead memory. 
-I get this idea from a blog https://pythonspeed.com/articles/python-integers-memory/. 
+# I suggest storing the list of data inside an array instead of a list, which can substantially reduce the overhead memory. 
+# I get this idea from a blog https://pythonspeed.com/articles/python-integers-memory/. 
 
 import numpy as ny
 import random
 
 arr = ny.array([float(random.randint(1,1000)) for _ in range(10000)])
-arr
-sys.getsizeof(arr)
+print(arr)
+print(sys.getsizeof(arr))
 # 80104
 
 l = [float(random.randint(1,1000)) for _ in range(10000)]
-l
-sys.getsizeof(l)
+print(l)
+print(sys.getsizeof(l))
 # 85176
 
-Compared to the size of list, an array has a much smaller sizes. Using an array can be useful when dealing with a large amount of data. 
+# Compared to the size of list, an array has a much smaller sizes. Using an array can be useful when dealing with a large amount of data. 
 
-# and suggest a strategy for calculating the average that would not require storing all the data in memory (7 points).
 
-I suggest calculating the average by looping through the total weight and number of weights one by one without storing them in a list. 
+# Suggest a strategy for calculating the average that would not require storing all the data in memory (7 points).
+
+# I suggest calculating the average by looping through the total weight and number of weights one by one without storing them in a list. 
 
 # Using my test file to illustrate the result:
 
@@ -68,9 +69,9 @@ with open('weights.txt') as f:
 print("average =", total_weight / count_weight)
 # average = 37.38430000000001
 
-sys.getsizeof(count_weight)
+print(sys.getsizeof(count_weight))
 # 28
-sys.getsizeof(total_weight)
+print(sys.getsizeof(total_weight))
 # 24
 
 # Using the code "my friend" provided:
@@ -82,6 +83,6 @@ with open('weights.txt') as f:
 print("average =", sum(weights) / len(weights))
 # average = 37.38430000000001
 
-We see here the sizes of total_weight and count_weight are just the single sizes of each elements. 
-We got the same result of average weight by not storing the data. Thus, this strategy will work. 
+# We see here the sizes of total_weight and count_weight are just the single sizes of each elements. 
+# We got the same result of average weight by not storing the data. Thus, this strategy will work. 
 
--- Scripts/HW2/HW2-Exercise4.py
+++ Scripts/HW2/HW2-Exercise4.py
@@ -6,68 +6,68 @@ import pandas as pd
 access_and_use_telemedicine = pd.read_csv('Access_and_Use_of_Telemedicine_During_COVID-19.csv') 
 # Downloaded from https://healthdata.gov/dataset/Access-and-Use-of-Telemedicine-During-COVID-19/c835-etjt. 
 access_and_use_telemedicine.head()
-len(access_and_use_telemedicine)  # 798
+print(len(access_and_use_telemedicine)) # 798
 
 # Describe the dataset (10 points) Your answer should address (but not be limited to): 
 # how many variables? 
 
-list(access_and_use_telemedicine.columns)
+prin(list(access_and_use_telemedicine.columns))
 # ['Round','Indicator','Group', 'Subgroup', 'Sample Size', 'Response', 'Percent', 'Standard Error', 'Suppression', 'Significant 1', 'Significant 2']
-len(list(access_and_use_telemedicine.columns))
+print(len(list(access_and_use_telemedicine.columns)))
 # There are 11 variables. 
 
 # Are the key variables explicitly specified or are they things you would have to derive (e.g. by inferring from text)? 
 
-The variables inside the dataset are straight-forward and explicily specified. "Groups" represent age, race, sex, education level, urbanization, and selected chronic conditions in the study. 
-"Subgroups" represent the subctegories within each group. For example, "18-44 years" is a subrgoup for age group. 
-For different purposes or the questions I will be interested in my final project, I might divide or group the age group into different age ranges. 
-For instance, if I am interested in knowing how compare middle-age people (before 65) and the elderly using telemedicine during COVID, I will group people from "18-44" and "44-64" together for analysis. 
+# The variables inside the dataset are straight-forward and explicily specified. "Groups" represent age, race, sex, education level, urbanization, and selected chronic conditions in the study. 
+# "Subgroups" represent the subctegories within each group. For example, "18-44 years" is a subrgoup for age group. 
+# For different purposes or the questions I will be interested in my final project, I might divide or group the age group into different age ranges. 
+# For instance, if I am interested in knowing how compare middle-age people (before 65) and the elderly using telemedicine during COVID, I will group people from "18-44" and "44-64" together for analysis. 
 
-The binary responses 0 and 1 under "Significance 1" and "Significance 2" indicates the estimated significance testing of difference  at the 5% significance level from Round 1 and Round 2, respetively. 
-I need to look up the description for the dataset (https://data.cdc.gov/NCHS/Access-and-Use-of-Telemedicine-During-COVID-19/8xy9-ubqz) to interpret thses responses. 
+# The binary responses 0 and 1 under "Significance 1" and "Significance 2" indicates the estimated significance testing of difference  at the 5% significance level from Round 1 and Round 2, respetively. 
+# I need to look up the description for the dataset (https://data.cdc.gov/NCHS/Access-and-Use-of-Telemedicine-During-COVID-19/8xy9-ubqz) to interpret thses responses. 
 
 # Are any of the variables exactly derivable from other variables? (i.e. are any of them redundant?) 
 
-There might be overlapped numbers of people into different subgroups. For example, there might be intersection of people who are Hispanic and aged between "18-44 years". 
+# There might be overlapped numbers of people into different subgroups. For example, there might be intersection of people who are Hispanic and aged between "18-44 years". 
 
 # Are there any variables that could in principle be statistically predicted from other variables? How many rows/data points are there? 
 
-All groups (i.e., age, race, sex, education level, urbanization, and selected chronic conditions) can serve as predictors for the variables "Response" of using telemedicine during COVID. 
+# All groups (i.e., age, race, sex, education level, urbanization, and selected chronic conditions) can serve as predictors for the variables "Response" of using telemedicine during COVID. 
 
-Age: 
+# Age: 
 age = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Age group'])
 print(f"There are {age} rows in age group")
 # There are 126 rows in age group
 
-Race:
+# Race:
 race = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Race/Hispanic origin'])
 print(f"There are {race} rows in race group")
 # There are 168 rows in race group
 
-Sex:
+# Sex:
 sex = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Sex'])
 print(f"There are {sex} rows in sex group")
 # There are 84 rows in sex group
 
-Education level:
+# Education level:
 education = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Education'])
 print(f"There are {education} rows in education level group")
 # There are 126 rows in education level group
 
-Urbanization:
+# Urbanization:
 urbanization = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Urbanization'])
 print(f"There are {urbanization} rows in urbanization level group")
 # There are 84 rows in urbanization level group
 
-Selected chronic conditions:
+# Selected chronic conditions:
 chronic_con = len(access_and_use_telemedicine[access_and_use_telemedicine['Group'] == 'Chronic conditions'])
 print(f"There are {chronic_con} rows in selected chronic conditions group")
 # There are 168 rows in selected chronic conditions group
 
 # Is the data in a standard format? If not, how could you convert it to a standard format?
 
-The data is in a standard format because each element is in their atomic form in a separate cell. For instance, the number of people responding using telemedicine in "18-44 years" are separated from those who age in "65 years and over". 
+# The data is in a standard format because each element is in their atomic form in a separate cell. For instance, the number of people responding using telemedicine in "18-44 years" are separated from those who age in "65 years and over". 
 
 # Describe the terms of use and identify any key restrictions (e.g. do you have to officially apply to get access to the data? Are there certain types of analyses you can't do?)
 
-This dataset is opened to public use so I do not haave to officially apply to get permission to download the data. 
+# This dataset is opened to public use so I do not haave to officially apply to get permission to download the data. 
